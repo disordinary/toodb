a889bbed2df9cfd9b42dc3344a407eefc4268df7
Ryan McCarvill <ryan@disordinary.com>
2016-12-12 23:19:04 +1300

    First reading from log file.

--- src/log.rs
+++ src/log.rs
@@ -3,54 +3,114 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use std::io::Seek;
 use std::io::SeekFrom;
+use std::io::BufReader;
+use std::io::Read;
+use std::str;
+use std::vec;
 
+extern crate serde_json;
+use self::serde_json::Value;
 
-#[derive(Debug)]
-pub struct Record {
+#[derive(Serialize, Deserialize, Debug)]
+pub enum CRUD {
+    create,
+    update,
+    delete
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Item {
+    id: String,
+    size: usize,
     offset: usize,
-    size: usize,
+    action: CRUD,
 }
 
-pub struct Table {
+pub struct Log {
     file: fs::File,
-    size: usize,
 }
 
-impl Table {
-    pub fn new(file_name: String) -> Table {
+impl Log {
+    pub fn new(file_name: String) -> Log {
         let mut option = OpenOptions::new();
         option.read(true);
         option.write(false);
         option.append(true);
         option.create(true);
-        let mut table = Table {
+        Log {
             file: option.open(file_name).unwrap(),
-            size: 0,
-        };
-        table.size = table.file.seek(SeekFrom::End(0)).unwrap() as usize;
-        table
+        }
     }
 
-    pub fn write(&mut self, comment: String) -> Record {
-        let mut bytes = comment.as_bytes();
-        let mut size = bytes.len();
-        let record = Record {
-            offset: self.size,
+    pub fn add(&mut self, id: String, offset: usize, size: usize, action: CRUD) {
+        let item = Item {
+            id: id,
             size: size,
+            offset: offset,
+            action: action,
         };
-        self.size += size;
-        self.file.write_all(bytes);
-        record
+        let mut json = serde_json::to_string(&item).unwrap();
+        let toOut = format!("{:0>4}{}", json.len(), json);
+        self.file.write_all(toOut.as_bytes());
     }
-    //
-    //    pub fn read(&mut self, offset: usize, size: usize) {
-    //        self.file.read
-    //    }
-}
+    pub fn add_new(&mut self, id: String, offset: usize, size: usize) {
+        self.add(id, offset, size, CRUD::create)
+    }
+    pub fn add_update(&mut self, id: String, offset: usize, size: usize) {
+        self.add(id, offset, size, CRUD::update)
+    }
+    pub fn add_delete(&mut self, id: String, offset: usize, size: usize) {
+        self.add(id, offset, size, CRUD::delete)
+    }
 
-fn main() {
-    let mut l = Table::new("test.txt".to_string());
-    let n = l.write("A☑".to_string());
-    println!("Array written is {:?}", n);
-}
+    pub fn read(&mut self) {
+        let mut buf = BufReader::new(&mut self.file);
+        let mut buffer = vec![0; 4];
+        buf.read(&mut buffer);
 
+        let result: &str;
+
+        unsafe {
+            result = str::from_utf8_unchecked(&buffer);
+        }
+        let size = result.parse::<usize>().unwrap();
+        let mut buffers = vec![0; size];
+        buf.read(&mut buffers);
+
+        let results: &str;
+
+        unsafe {
+            results = str::from_utf8_unchecked(&buffers);
+        }
+        println!("{:?}",results);
+
+       // self._read(3);
+//        let my_int = size.parse::<usize>().unwrap();
+//
+//        println!("{:?}",my_int);
+//
+//        let x = self._read(2);
+//        println!("{:?}",x);
+    }
+
+//    pub fn _read>(&mut self, size: usize) -> String {
+//        return "TEST".to_string();
+//    }
+//    fn _read<R: Read>(&self, mut buf: R, size: usize) -> String {
+//
+//        //let mut buffer: [u8; size] = [0;size];
+//        // let mut buffer = Vec::with_capacity(size);
+//        let mut buffer = vec![0; size];
+//        buf.read(&mut buffer);
+//        let result: &str;
+//
+//        unsafe {
+//            result = str::from_utf8_unchecked(&buffer);
+//        }
+//
+//         println!("{:?}",result);
+//
+//        result.to_string()
+//    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+#![feature(proc_macro)]
+
+#[macro_use]
+extern crate serde_derive;
 mod table;
+mod log;
 
 fn main() {
-    let mut l = table::Table::new("test.txt".to_string());
-    let n = l.write("A☑".to_string());
-    println!("Array written is {:?}", n);
+//    let mut l = table::Table::new("test.txt".to_string());
+//    let n = l.write("A☑".to_string());
+//    println!("Array written is {:?}", n);
+    let mut ll = log::Log::new("test.log".to_string());
+    ll.read();
+    //ll.read();
+    //println!("{:?}", ll.add_new("TEST🍻".to_string(), 100, 100));
 }
 
